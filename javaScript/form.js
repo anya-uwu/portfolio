b94f44e5fc257d6ad849ae832c95c21692e8b96f
anya-uwu <anyadev@proton.me>
2023-08-24 18:45:11 -0400

    adding new portfolio items and working on contact form

---
+++ javaScript/form.js
@@ -0,0 +1,11 @@
+// logic for form goes here
+
+/* 
+- prevent default
+- on click we need to:
+    - check if email is valid
+    - check if message is valid
+*/
+
+const button = document.getElementById('submit');
+console.log(button);
